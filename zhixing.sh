9b2b37893e35cb12a250312ca0c2384944f166c3
肥羊 <yunspeedtest@gmail.com>
2021-11-28 03:18:24 +0800

    Update zhixing.sh

--- zhixing.sh
+++ zhixing.sh
@@ -2,8 +2,9 @@
 
 #文件路径，最新ip写入lastwanip.txt 
 iptxt="/home/lastwanip.txt"
-#获取WANIP接口
+#OpenWrt获取WANIP接口
 newwanip=`ubus call network.interface.wan status | grep \"address\" | grep -oE '[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}'`
+#NAS或其他Linux系统需替换为：newwanip=`wget http://members.3322.org/dyndns/getip -q -O -`
 #如接口获取不到ip，本次取消操作
 echo $newwanip
 if [ x"$newwanip" = "x" ]; then
